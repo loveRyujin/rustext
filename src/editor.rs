864068e5804313dacfe760c5f552bc6d0a13c634
lichen <1725350830@qq.com>
2025-10-31 23:56:40 +0800

    refactor: Refactor terminal I/O into Editor struct
    - Create a new Editor struct with run method for terminal input handling
    - Move terminal I/O logic from main to Editor::run method
    - Simplify main function by using the new Editor struct
    
    Signed-off-by: lichen <1725350830@qq.com>

---
+++ src/editor.rs
@@ -0,0 +1,31 @@
+use std::io::{self, Read};
+use crossterm::terminal::enable_raw_mode;
+use crossterm::terminal::disable_raw_mode;
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn new() -> Self {
+        Editor{}
+    }
+
+    pub fn run(&self) {
+        enable_raw_mode().unwrap();
+
+        for b in io::stdin().bytes() {
+            let b = b.unwrap();
+            let c = b as char;
+            
+            if c.is_control() {
+                println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+            } else {
+                println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+            }
+
+            if c == 'q' {
+                disable_raw_mode().unwrap();
+                break;
+            }
+        }
+    }
+}
